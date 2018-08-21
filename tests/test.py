2d0103039895f25c8a77320f3b63c0bc4bdf37d5
Brendan Dwyer <brendan.dwyer@ibm.com>
2018-08-21 16:22:45 -0700

    added tests changed wget to show progress changed to codait/max-base image added . ignore files

---
+++ tests/test.py
@@ -0,0 +1,28 @@
+import pytest
+import pycurl
+import io
+import json
+
+
+def test_response():
+    c = pycurl.Curl()
+    b = io.BytesIO()
+    c.setopt(pycurl.URL, 'http://localhost:5000/model/predict')
+    c.setopt(pycurl.HTTPHEADER, ['Accept:application/json', 'Content-Type: multipart/form-data'])
+    c.setopt(pycurl.HTTPPOST, [('image', (pycurl.FORM_FILE, "assets/burger.jpg"))])
+    c.setopt(pycurl.WRITEFUNCTION, b.write)
+    c.perform()
+    assert c.getinfo(pycurl.RESPONSE_CODE) == 200
+    c.close()
+
+    response = b.getvalue()
+    response = json.loads(response)
+
+    assert response['status'] == 'ok'
+    assert response['predictions'][0]['label_id'] == 'n07697313'
+    assert response['predictions'][0]['label'] == 'cheeseburger'
+    assert response['predictions'][0]['probability'] > 0.75
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
