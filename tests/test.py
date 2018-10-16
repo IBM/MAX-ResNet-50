97284bf095f5d37e0e51680d74986bf57b774c03
Brendan Dwyer <brendan.dwyer@ibm.com>
2018-10-16 14:08:56 -0700

    Test other endpoints - test other endpoints - increased sleep in .travis.yml just to be safe - check md5 hash of downloaded file

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,34 @@ import pytest
 import requests
 
 
-def test_response():
+def test_swagger():
+
+    model_endpoint = 'http://localhost:5000/swagger.json'
+
+    r = requests.get(url=model_endpoint)
+    assert r.status_code == 200
+    assert r.headers['Content-Type'] == 'application/json'
+
+    json = r.json()
+    assert 'swagger' in json
+    assert json.get('info') and json.get('info').get('title') == 'Model Asset Exchange Server'
+
+
+def test_metadata():
+
+    model_endpoint = 'http://localhost:5000/model/metadata'
+
+    r = requests.get(url=model_endpoint)
+    assert r.status_code == 200
+
+    metadata = r.json()
+    assert metadata['id'] == 'resnet50-keras-imagenet'
+    assert metadata['name'] == 'resnet50 Keras Model'
+    assert metadata['description'] == 'resnet50 Keras model trained on ImageNet'
+    assert metadata['license'] == 'MIT'
+
+
+def test_predict():
     model_endpoint = 'http://localhost:5000/model/predict'
     file_path = 'assets/burger.jpg'
 
