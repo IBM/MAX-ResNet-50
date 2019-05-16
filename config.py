56129f3ef8a0d78978b6db056cc6972772c9f765
saishruthi <saishruthi.tn@ibm.com>
2019-05-16 11:28:43 -0700

    Title change

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ RESTPLUS_MASK_SWAGGER = False
 SWAGGER_UI_DOC_EXPANSION = 'none'
 
 # API metadata
-API_TITLE = 'MAX ResNet 50'
+API_TITLE = 'MAX Image Classifier - ResNet 50'
 API_DESC = 'Identify objects in images using a first-generation deep residual network.'
 API_VERSION = '1.1.0'
 
