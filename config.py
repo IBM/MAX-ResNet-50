b0614ae7629c64978d162fa6600b9a1bb6954a51
Nick Pentreath <nickp@za.ibm.com>
2019-07-05 11:39:31 +0200

    Bump version in prep for next release

--- config.py
+++ config.py
@@ -26,6 +26,6 @@ SWAGGER_UI_DOC_EXPANSION = 'none'
 # API metadata
 API_TITLE = 'MAX Image Classifier - ResNet 50'
 API_DESC = 'Identify objects in images using a first-generation deep residual network.'
-API_VERSION = '1.1.0'
+API_VERSION = '1.2.0'
 
 DEFAULT_MODEL_PATH = 'assets/resnet50.h5'
